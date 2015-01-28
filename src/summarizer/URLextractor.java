9dd019f71db56fb58c6505bb4c6767b927b2b9bf
kalyanadupa <abhi.klyn@gmail.com>
2015-01-28 17:44:11 +0530

    removed jSoup, added all required lib

--- src/summarizer/URLextractor.java
+++ src/summarizer/URLextractor.java
@@ -10,12 +10,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
-
-import org.jsoup.select.Elements;
 
 /**
  *
@@ -25,39 +20,15 @@ public class URLextractor {
 
     
     List<String> textFromURL(URL u){
-        Document doc;
+        
         List<String> outputStrings = new ArrayList<String>();
-        try {
-
-            // need http protocol
-            String text = Jsoup.parse(u, 10000).text();
-            System.out.println(text);
-
-            String[] tokenizedStrings = text.split("\\.");
-            for (String x : tokenizedStrings) {
-                if (x.compareTo("") != 0) {
-                    outputStrings.add(x);
-                }
+        String text = "";
+        System.out.println(text);
+        String[] tokenizedStrings = text.split("\\.");
+        for (String x : tokenizedStrings) {
+            if (x.compareTo("") != 0) {
+                outputStrings.add(x);
             }
-            // get page title
-//            doc = Jsoup.connect("http://timesofindia.indiatimes.com/india/Modis-drug-speech-puts-Punjab-government-in-a-spot/articleshow/45516872.cms").get();
-//            String title = doc.title();
-//            System.out.println("title : " + title);
-//
-//            // get all links
-//            Elements links = doc.select("a[href]");
-//            
-//            for (Element link : links) {
-//
-//                // get the value from href attribute
-//                System.out.println("\nlink : " + link.attr("href"));
-//                System.out.println("text : " + link.text());
-//
-//            }
-
-        }
-        catch (IOException e) {
-            e.printStackTrace();
         }
         return outputStrings;
     }
